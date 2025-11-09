eacd5a2643593a28c21c8c18a3fa72e6e37d4110
ikrishra <krishnarathore.me@gmail.com>
2025-11-09 10:09:29 +0530

    404 pade added

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </div>
